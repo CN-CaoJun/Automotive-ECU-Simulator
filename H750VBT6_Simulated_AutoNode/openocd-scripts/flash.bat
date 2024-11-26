b8e2ff1da507748a9f760024e28da702adca94c3
Cao Jun <sxha951@gmail.com>
2024-11-26 16:44:22 +0800

    [DEBUG]ENable openocd debug
    Description of Change:
           openocd and pyocd both work well, openocd has the fast speed
    Author:
    	sxha951@gmail.com
    Reviewer:
    	sxha951@gmail.com
    Test:
    	Pass

--- H750VBT6_Simulated_AutoNode/openocd-scripts/flash.bat
+++ H750VBT6_Simulated_AutoNode/openocd-scripts/flash.bat
@@ -1,3 +1,3 @@
 @echo off
-openocd -f interface/cmsis-dap.cfg -f target/stm32h7x.cfg  -c "program  ../Debug/rtthread.bin  verify reset exit 0x08000000"
+openocd -f interface/cmsis-dap.cfg -f target/stm32h7x.cfg  -c "program  ../build/Simulated-GW-ECU.bin  verify reset exit 0x08000000"
 pause
