b6daaecad0259d4a9e92a16651373515d8ce1b13
Cao Jun <sxha951@gmail.com>
2024-12-11 18:45:31 +0800

    [DEBUG]add info of clk

--- H750VBT6_Simulated_AutoNode/applications/main.c
+++ H750VBT6_Simulated_AutoNode/applications/main.c
@@ -15,9 +15,35 @@
 #include <rtdbg.h>
 
 extern int rtcan_node_start(void);
+static void timer_callback(void *parameter)
+{
+    static int count = 0;
+    count++;
+    // rt_kprintf("Timer test %d \n", count);
+}
 
 int main(void)
 {
     rtcan_node_start();
+    rt_timer_t timer = rt_timer_create("timer1",  
+                                       timer_callback,  
+                                       RT_NULL,  
+                                       100,  
+                                       RT_TIMER_FLAG_PERIODIC);  
+
+    if (timer != RT_NULL)
+    {
+        rt_timer_start(timer);
+    }
+    else
+    {
+        rt_kprintf("Failed to create timer\n");
+    }
+
+    while (1)
+    {
+        rt_thread_mdelay(1000); 
+    }
+
     return RT_EOK;
 }
